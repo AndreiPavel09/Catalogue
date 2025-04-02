a0b6f34ca4cc1fe49e76f22b268af75583f19b51
BanicaCristian04 <cristian-andrei.banica@student.unitbv.ro>
2025-04-03 00:34:04 +0300

    MC-3

---
+++ Backend/Repositories/Implementations/StudentRepository.cs
@@ -0,0 +1,52 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Backend.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Repositories.Implementations
+{
+        public class StudentRepository : IStudentRepository
+        {
+            private readonly ApplicationDbContext _context;
+
+            public StudentRepository(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Student>> GetAllStudentsAsync()
+            {
+                return await _context.Students.ToListAsync();
+            }
+
+            public async Task<Student> GetStudentByIdAsync(int id)
+            {
+                return await _context.Students.FindAsync(id);
+            }
+
+            public async Task<Student> CreateStudentAsync(Student student)
+            {
+                await _context.Students.AddAsync(student);
+                await _context.SaveChangesAsync();
+                return student;
+            }
+
+            public async Task UpdateStudentAsync(Student student)
+            {
+                _context.Entry(student).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+
+            public async Task<bool> DeleteStudentAsync(int id)
+            {
+                var student = await GetStudentByIdAsync(id);
+                if (student == null)
+                {
+                    return false;
+                }
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+}
---
+++ Backend/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Models;
+
+namespace Backend.Repositories.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetAllStudentsAsync();
+        Task<Student> GetStudentByIdAsync(int id);
+        Task<Student> CreateStudentAsync(Student student);
+        Task UpdateStudentAsync(Student student);
+        Task<bool> DeleteStudentAsync(int id);
+    }
+}
